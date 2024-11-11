65ff6d4053909c680c6a4c6a5051526e484ec9ec
hamzaMeski <meskihamza5@gmail.com>
2024-11-12 00:34:19 +0100

    core controllers endpoints setup

---
+++ BACKEND/src/main/java/com/WaitingList/BACKEND/controller/v1/WaitingRoomController.java
@@ -0,0 +1,30 @@
+package com.WaitingList.BACKEND.controller.v1;
+
+import com.WaitingList.BACKEND.dto.request.WaitingRoomRequestDTO;
+import com.WaitingList.BACKEND.dto.response.WaitingRoomResponseDTO;
+import com.WaitingList.BACKEND.service.WaitingRoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/waiting-rooms")
+public class WaitingRoomController {
+    private final WaitingRoomService waitingRoomService;
+
+    @PostMapping
+    public ResponseEntity<WaitingRoomResponseDTO> create(@RequestBody WaitingRoomRequestDTO requestDTO) {
+        return ResponseEntity.ok(waitingRoomService.create(requestDTO));
+    }
+
+    @GetMapping("/today")
+    public ResponseEntity<WaitingRoomResponseDTO> getTodayWaitingRoom() {
+        return ResponseEntity.ok(waitingRoomService.getOrCreateTodayWaitingRoom());
+    }
+
+    @PatchMapping("/{id}/algorithm")
+    public ResponseEntity<WaitingRoomResponseDTO> updateAlgorithm(
+            @PathVariable Long id,
+            @RequestParam SchedulingAlgorithm algorithm) {
+        return ResponseEntity.ok(waitingRoomService.updateAlgorithm(id, algorithm));
+    }
+}
