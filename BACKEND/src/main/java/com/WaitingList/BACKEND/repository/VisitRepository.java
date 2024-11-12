ce1816fa35460db39a7c641497dfa0680ddd619c
hamzaMeski <meskihamza5@gmail.com>
2024-11-12 16:11:32 +0100

    repositories methods

--- BACKEND/src/main/java/com/WaitingList/BACKEND/repository/VisitRepository.java
+++ BACKEND/src/main/java/com/WaitingList/BACKEND/repository/VisitRepository.java
@@ -4,12 +4,20 @@ import com.WaitingList.BACKEND.entity.Visit;
 import com.WaitingList.BACKEND.util.constants.VisitorStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
-    List<Visit> findByWaitingRoomIdAndStatus(Long waitingRoomId, VisitorStatus status);
+    // For queue management
     List<Visit> findByWaitingRoomIdOrderByArrivalTimeAsc(Long waitingRoomId);
     List<Visit> findByWaitingRoomIdOrderByPriorityDescArrivalTimeAsc(Long waitingRoomId);
     List<Visit> findByWaitingRoomIdOrderByEstimatedProcessingTimeAscArrivalTimeAsc(Long waitingRoomId);
+
+    // For capacity checking
+    List<Visit> findByWaitingRoomIdAndStatus(Long waitingRoomId, VisitorStatus status);
+
+    // For current visit
+    Optional<Visit> findByVisitorIdAndStatusIn(Long visitorId, List<VisitorStatus> statuses);
 }
--- BACKEND/src/main/java/com/WaitingList/BACKEND/repository/WaitingRoomRepository.java
+++ BACKEND/src/main/java/com/WaitingList/BACKEND/repository/WaitingRoomRepository.java
@@ -3,6 +3,7 @@ package com.WaitingList.BACKEND.repository;
 import com.WaitingList.BACKEND.entity.WaitingRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.time.LocalDate;
 import java.util.Optional;
 
